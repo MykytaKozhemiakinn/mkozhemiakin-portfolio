7edc09b5a305bac1b61b5751173a153d09e79018
mKozhemiakin <m.kozhemiakin@cez.gov.pl>
2023-12-20 16:21:08 +0100

    fix lint

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {DockModule} from "primeng/dock";
-import {NgOptimizedImage} from "@angular/common";
-import {navbarConfig} from "@app/constants/navbar-config";
-import {MenuItem} from "primeng/api";
+import { DockModule } from 'primeng/dock';
+import { NgOptimizedImage } from '@angular/common';
+import { navbarConfig } from '@app/constants/navbar-config';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    DockModule,
-    NgOptimizedImage
-  ],
+  imports: [DockModule, NgOptimizedImage],
   templateUrl: './navbar.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
   public readonly navbarConfig: MenuItem[] = navbarConfig;
--- src/app/constants/navbar-config.ts
+++ src/app/constants/navbar-config.ts
@@ -1,4 +1,4 @@
-import {MenuItem} from "primeng/api";
+import { MenuItem } from 'primeng/api';
 
 export const navbarConfig: MenuItem[] = [
   {
@@ -6,34 +6,35 @@ export const navbarConfig: MenuItem[] = [
     icon: 'assets/icons/home-icon.svg',
     routerLink: '/',
     tooltipOptions: {
-      tooltipPosition: "top",
+      tooltipPosition: 'top',
       tooltipLabel: 'Home',
-    }
+    },
   },
   {
     label: 'About me',
     icon: 'assets/icons/about-me-icon.svg',
     routerLink: '/about-me',
     tooltipOptions: {
-      tooltipPosition: "top",
+      tooltipPosition: 'top',
       tooltipLabel: 'About me',
-    }
+    },
   },
   {
     label: 'Projects',
     icon: 'assets/icons/projects-icon.svg',
     routerLink: '/projects',
     tooltipOptions: {
-      tooltipPosition: "top",
+      tooltipPosition: 'top',
       tooltipLabel: 'Projects',
-    }
+    },
   },
   {
     label: 'Contact',
     icon: 'assets/icons/contact-me-icon.svg',
-    routerLink: '/contact', tooltipOptions: {
-      tooltipPosition: "top",
+    routerLink: '/contact',
+    tooltipOptions: {
+      tooltipPosition: 'top',
       tooltipLabel: 'Contact',
-    }
-  }
-]
+    },
+  },
+];
